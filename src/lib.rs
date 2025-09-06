7943a1bedfb291de48fb7566d6b151b3f31a7f11
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2025-09-06 01:25:15 -0600

    docs: update README and lib.rs with usage and feature details for oneDNN

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,38 @@
 //
 // http://opensource.org/licenses/mit-license.php
 
+//! This crate provides the source build of
+//! [oneAPI Deep Neural Network Library (oneDNN)](https://!github.com/uxlfoundation/oneDNN) for Rust projects.
+//!
+//! ## Usage
+//!
+//! Add this crate to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! onednn-src = "0.1.0"
+//! ```
+//!
+//! Then, add the extern declaration to your `main.rs` or `lib.rs` to ensure the libraries are properly linked:
+//!
+//! ```rust
+//! extern crate onednn_src;
+//! ```
+//!
+//! This crate also exports the `DEP_DNNL_ROOT` environment variable,
+//! which points to the root directory of the compiled oneDNN library.
+//! You can use this in your `build.rs` to locate the library.
+//!
+//! ## Features
+//!
+//! - `graph`: Enables the graph component.
+//! - `training`: Enables training functionality.
+//! - `inference`: Enables inference functionality.
+//! - `experimental`: Enables [experimental features](https://!uxlfoundation.github.io/oneDNN/dev_guide_experimental.html#doxid-dev-guide-experimental)
+//! - `verbose`: Enables [verbose mode](https://!uxlfoundation.github.io/oneDNN/dev_guide_verbose.html#doxid-dev-guide-verbose)
+//!
+//! By default, the `training` feature is enabled.
+//! The `training` and `inference` features are mutually exclusive.
+//! If neither is specified, `training` will be used.
+
 #![no_std]
